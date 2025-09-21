657f42de9af0a33f1f06a3ee105ab28ec223a9fc
Michi Saito <ms20054869tv@gmail.com>
2025-09-21 16:47:48 +0900

    エラー

--- fuel/app/config/crypt.php
+++ fuel/app/config/crypt.php
@@ -3,4 +3,9 @@ return array (
   'sodium' => array(  
     'cipherkey' => '0a045e458c1384bfebe5ed99c60786058562b279cc85698435d4a3c24568374c',
   ),
+  'legacy' => array(
+    'crypto_key' => 'CrBeY7P63uGP/tJEZniMAQ==', // 16バイト（128ビット）
+    'crypto_hmac' => 'jHGWQS6iwRbZ1eAkVCO6Xw==', // 16バイト（128ビット）
+    'crypto_iv' => '/BDrem6ySoiN4srSj/vm4Q==', // 16バイト（128ビット）
+  ),
 );
--- fuel/app/config/session.php
+++ fuel/app/config/session.php
@@ -3,8 +3,10 @@ return array(
     'driver'        => 'cookie',
     'cookie_name'   => 'fuelcid',
     'expiration_time' => 7200,
-    'encrypt_cookie'  => false,
+    'encrypt_cookie'  => true,
     'validate'      => array(),
     'auto_initialize' => true,
     'auto_start'    => true,
+    'secure'        => false, // HTTPS環境では true に設定
+    'httponly'      => true,
 );
