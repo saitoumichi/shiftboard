4d2880c4a23b99814bf9b69ec8e126887294f10c
Michi Saito <ms20054869tv@gmail.com>
2025-09-22 19:05:41 +0900

    README

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -15,9 +15,9 @@ return [
   // API（JSON）
   'api/shifts'                    => 'api/shifts/index',       // GET一覧 / POST作成
   'api/shifts/(:num)'             => 'api/shifts/show/$1',     // GET詳細 / PUT更新 / DELETE削除
-  'api/shifts/(:num)/join'        => 'api/shift_assignments/join/$1',   // POST参加
-  'api/shifts/(:num)/cancel'      => 'api/shift_assignments/cancel/$1', // POSTキャンセル
-  'api/shift_assignments'         => 'api/shift_assignments/index',     // GET一覧
+  'api/shifts/(:num)/join'        => 'api/shift/assignments/join/$1',   // POST参加
+  'api/shifts/(:num)/cancel'      => 'api/shift/assignments/cancel/$1', // POSTキャンセル
+  'api/shift_assignments'         => 'api/shift/assignments/index',     // GET一覧
   'api/users'                     => 'api/users/index',        // GET/POST
   'api/users/(:num)'              => 'api/users/show/$1',      // GET/PUT/DELETE
   'api/debug/db-test'           => 'api/debug/db_test',
